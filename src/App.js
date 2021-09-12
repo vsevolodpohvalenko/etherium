bb1e63e858c587c848ffa79e3ef3bc6ea616db4f
vsevolodpohvalenko <vsevolod.pohvalenko@gmail.com>
2021-09-12 12:32:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+    useEffect(() => {
+
+        (async function () {
+
+            const provider = await detectEthereumProvider();
+
+            if (provider) {
+                console.log("Hello")// Initialize your app
+            } else {
+                console.log("Provider")
+                console.log('Please install MetaMask!');
+            }
+
+        })();
+    }, []);
+
+    async function onInit() {
+        await window.ethereum.enable();
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        console.log(account)
+        window.ethereum.on('accountsChanged', function (accounts) {
+            // Time to reload your interface with accounts[0]!
+            console.log(accounts[0])
+        });
+    }
+
+    onInit();
+
+    return (
+        <div className="App">
+            <div>Hello World</div>
+
+        </div>
+    );
 }
 
 export default App;
